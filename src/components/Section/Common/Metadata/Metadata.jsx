9c244526735b33fe940c4f7555c16e41289ea184
gary <harshprakash000@gmail.com>
2024-09-01 18:28:43 +0530

    POV completed

---
+++ src/components/Section/Common/Metadata/Metadata.jsx
@@ -0,0 +1,11 @@
+// metadata.js
+export const Metadata = {
+    title: "CargoWave || Logistics & Transport",
+    description: "CargoWave is the well known logistics and transport company",
+    icons: {
+      icon: ["/favicon.ico?v=4"],
+      apple: ["/favicon.ico?v=4"],
+      shortcut: ["/favicon.ico"],
+    },
+  };
+  
